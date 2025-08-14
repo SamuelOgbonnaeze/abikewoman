7f16953d7b106934081761dab11f773671812c3c
samuel.ogbonna-eze <samuel.ogbonna-eze@moniepoint.com>
2025-08-15 00:05:37 +0100

    (feat) new commit

--- app/anu-collection/[collectionId]/page.tsx
+++ app/anu-collection/[collectionId]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { useParams } from "next/navigation";
+import Container from "@/components/container";
+import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 
 const CollectionPage = () => {
     const { collectionId } = useParams();
@@ -10,8 +13,20 @@ const CollectionPage = () => {
 
 
     return(
-        <div>
-            oijhhui hello world
+        <div className="bg-white">
+            <Container>
+                <div className="px-4 py-10 sm:px-6 lg:px-8">
+                    <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+                        {/* Gallery */}
+                        <Gallery images={collectionData.images} />
+                        <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0 ">
+                            {/* Info */}
+                            <Info data={collectionData} />
+                        </div>
+                    </div>
+                </div>
+            </Container>
+
         </div>
     )
 }
@@ -21,56 +36,88 @@ export default CollectionPage;
 const anuCollectionData = [
     {
         id: "anu-1",
-        imageSrc: "image 114.png",
+        images: [
+            { id: "1", url: "/hero_4.jpg" },
+            { id: "2", url: "/hero_3.jpg" },
+            { id: "3", url: "/hero_2.jpg" },
+        ],
         title: "Anu Collection Title",
         description: "Anu Collection Description goes here. It can be a bit longer to test the line clamping.",
         link: "anu-collection/anu-1",
     },
     {
         id: "anu-2",
-        imageSrc: "image 114.png",
+        images: [
+            { id: "1", url: "/hero_4.jpg" },
+            { id: "2", url: "/hero_3.jpg" },
+            { id: "3", url: "/hero_2.jpg" },
+        ],
         title: "Anu Collection Title",
         description: "Anu Collection Description goes here. It can be a bit longer to test the line clamping.",
         link: "anu-collection/anu-2",
     },
     {
         id: "anu-3",
-        imageSrc: "image 114.png",
+        images: [
+            { id: "1", url: "/hero_4.jpg" },
+            { id: "2", url: "/hero_3.jpg" },
+            { id: "3", url: "/hero_2.jpg" },
+        ],
         title: "Anu Collection Title",
         description: "Anu Collection Description goes here. It can be a bit longer to test the line clamping.",
         link: "anu-collection/anu-3",
     },
     {
         id: "anu-4",
-        imageSrc: "image 114.png",
+        images: [
+            { id: "1", url: "/hero_4.jpg" },
+            { id: "2", url: "/hero_3.jpg" },
+            { id: "3", url: "/hero_2.jpg" },
+        ],
         title: "Anu Collection Title",
         description: "Anu Collection Description goes here. It can be a bit longer to test the line clamping.",
         link: "anu-collection/anu-4",
     },
     {
         id: "anu-5",
-        imageSrc: "image 114.png",
+        images: [
+            { id: "1", url: "/hero_4.jpg" },
+            { id: "2", url: "/hero_3.jpg" },
+            { id: "3", url: "/hero_2.jpg" },
+        ],
         title: "Anu Collection Title",
         description: "Anu Collection Description goes here. It can be a bit longer to test the line clamping.",
         link: "anu-collection/anu-5",
     },
     {
         id: "anu-6",
-        imageSrc: "image 114.png",
+        images: [
+            { id: "1", url: "/hero_4.jpg" },
+            { id: "2", url: "/hero_3.jpg" },
+            { id: "3", url: "/hero_2.jpg" },
+        ],
         title: "Anu Collection Title",
         description: "Anu Collection Description goes here. It can be a bit longer to test the line clamping.",
         link: "anu-collection/anu-6",
     },
     {
         id: "anu-7",
-        imageSrc: "image 114.png",
+        images: [
+            { id: "1", url: "/hero_4.jpg" },
+            { id: "2", url: "/hero_3.jpg" },
+            { id: "3", url: "/hero_2.jpg" },
+        ],
         title: "Anu Collection Title",
         description: "Anu Collection Description goes here. It can be a bit longer to test the line clamping.",
         link: "anu-collection/anu-7",
     },
     {
         id: "anu-8",
-        imageSrc: "image 114.png",
+        images: [
+            { id: "1", url: "/hero_4.jpg" },
+            { id: "2", url: "/hero_3.jpg" },
+            { id: "3", url: "/hero_2.jpg" },
+        ],
         title: "Anu Collection Title",
         description: "Anu Collection Description goes here. It can be a bit longer to test the line clamping.",
         link: "anu-collection/anu-8",
