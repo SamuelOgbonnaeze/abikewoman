639def30b4478d23fb511976ec662e154b56a59d
samuel.ogbonna-eze <samuel.ogbonna-eze@moniepoint.com>
2025-08-14 23:33:53 +0100

    (feat) update build

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,33 @@
 import AdBanner from "@/components/ad-banner";
-import {AnuCollection} from "@/components/anu-collection";
+import { AnuCollection } from "@/components/anu-collection";
 import Banner from "@/components/banner";
-import {BridalsCollection} from "@/components/bridals";
+import { BridalsCollection } from "@/components/bridals";
+import Footer from "@/components/footer";
 import Hero from "@/components/hero";
-import {IreCollection} from "@/components/ire-collection";
-import {ReadyToWear} from "@/components/ready-to-wear";
+import { IreCollection } from "@/components/ire-collection";
+import { Navigation } from "@/components/navigation";
+import { ReadyToWear } from "@/components/ready-to-wear";
 
 
 export default function Home() {
-    return (
-        <div className="text-3xl">
-            <Hero/>
-            <div className="px-[83px] lg:px-[272px] mb-[48px]">
-                <AdBanner/>
-            </div>
-            <div className='flex-col space-y-[65px]'>
-                <IreCollection/>
-                <BridalsCollection/>
-                <Banner/>
-                <AnuCollection/>
-                <ReadyToWear/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="text-3xl">
+      <Navigation />
+      <Hero />
+
+      <div className="px-[83px] lg:px-[272px] mb-[48px]">
+        <AdBanner />
+      </div>
+      <div className='flex-col space-y-[65px]'>
+        <IreCollection />
+        <BridalsCollection />
+        <Banner />
+        <AnuCollection />
+        <ReadyToWear />
+      </div>
+
+      <Footer />
+
+    </div>
+  );
 }
