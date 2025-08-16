3f13b2dfc5f0676906dfa03629995bac92b91608
samuel.ogbonna-eze <samuel.ogbonna-eze@moniepoint.com>
2025-08-16 10:05:29 +0100

    (feat) new commit

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -15,7 +15,7 @@ const Gallery: React.FC<GalleryProps> = ({
 }) => {
     return (
         <TabGroup as="div" className="flex flex-col-reverse" >
-            <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+            <div className="mx-auto mt-6 w-full max-w-2xl block lg:max-w-none">
                 <TabList className="grid grid-cols-4 gap-6">
                     {images.map((image) => (
                         <GalleryTab key={image.id} image={image} />
