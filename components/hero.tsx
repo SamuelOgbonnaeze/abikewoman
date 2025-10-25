01c68eb40587f03c95f4be1944e17522a3946a03
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2025-10-25 06:41:03 +0100

    hide subtext on hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -54,11 +54,12 @@ const Hero = () => {
                 <p className="text-[16px] md:text-[24px] lg:text-[44px] font-openSans leading-[22px] md:leading-[36px] lg:leading-[62px] font-bold">
                   The AbikeWoman
                 </p>
-                <div className="w-[319px] md:w-[522px] lg:w-[682px] lg:py-1 lg:px-4">
+                {/* Subtext here */}
+                {/* <div className="w-[319px] md:w-[522px] lg:w-[682px] lg:py-1 lg:px-4">
                   <p className="font-openSans font-normal text-[8px] lg:text-[16px] leading-[14px] lg:leading-[28px]">
                     Subtitle text
                   </p>
-                </div>
+                </div> */}
               </div>
             </div>
           </SwiperSlide>
