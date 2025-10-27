2e1c2e97d66842e1f16f878bebc6df2c923057a1
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2025-10-27 07:36:01 +0100

    ring on the select on focus disabled

--- components/ui/native-select.tsx
+++ components/ui/native-select.tsx
@@ -15,7 +15,7 @@ function NativeSelect({ className, ...props }: React.ComponentProps<"select">) {
           "text-white placeholder:text-gray-300 selection:bg-primary selection:text-primary-foreground",
           "dark:bg-input/30 dark:hover:bg-input/50",
           "h-9 w-full min-w-0 appearance-none rounded-md bg-transparent px-3 py-2 pr-9 text-sm shadow-xs transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed",
-          "focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+          // "focus-visible:ring-ring/30 focus-visible:ring-[3px]",
           "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
           className
         )}
