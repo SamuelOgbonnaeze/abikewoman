d035ef72ae3b658334ced13a7c2dd3e1f01406d5
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2025-10-29 16:53:10 +0100

    Text removed on hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -50,17 +50,17 @@ const Hero = () => {
               />
 
               {/* Text content */}
-              <div className="relative w-full h-full flex flex-col items-center justify-center z-20 text-gray-100 text-center gap-y-1">
+              {/* <div className="relative w-full h-full flex flex-col items-center justify-center z-20 text-gray-100 text-center gap-y-1">
                 <p className="text-[16px] md:text-[24px] lg:text-[44px] font-openSans leading-[22px] md:leading-[36px] lg:leading-[62px] font-bold">
                   The AbikeWoman
                 </p>
-                {/* Subtext here */}
-                {/* <div className="w-[319px] md:w-[522px] lg:w-[682px] lg:py-1 lg:px-4">
+                Subtext here
+                <div className="w-[319px] md:w-[522px] lg:w-[682px] lg:py-1 lg:px-4">
                   <p className="font-openSans font-normal text-[8px] lg:text-[16px] leading-[14px] lg:leading-[28px]">
                     Subtitle text
                   </p>
-                </div> */}
-              </div>
+                </div>
+              </div> */}
             </div>
           </SwiperSlide>
         ))}
