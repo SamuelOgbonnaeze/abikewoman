a61bb50af73eb4cc043882803888c7d73c4cfc9a
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2025-10-31 15:01:03 +0100

    layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,13 +26,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <Toaster />
         <Navigation />
-        {children}
+        <main className="flex-grow w-full">{children}</main>
         <Footer />
       </body>
     </html>
